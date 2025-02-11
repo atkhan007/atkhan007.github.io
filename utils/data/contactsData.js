328de2a1ad9634e7885bb360e83cd40077d6ea70
Abdullah Tahir <atkhan007@hotmail.com>
2025-02-11 21:39:10 +0100

    init

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'atkhan007@protonmail.com',
+    //phone: '+8801608797655',
+    address: 'Cologne, Germany',
+    github: 'https://github.com/atkhan007',
+    //facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    linkedIn: 'https://www.linkedin.com/in/atkan007/',
+    //twitter: 'https://twitter.com/said7388',
+    //stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    //devUsername: "said7388"
+}
---
+++ utils/data/experience.js
@@ -0,0 +1,41 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Hybrid Cloud Architect',
+    company: "IBM Deutschland GmbH",
+    duration: "(2022 - Present)",
+    tasks: [
+      " Implemented IBM Cortex AI RAG system for LLM chatbot knowledge bases for enterprise clients.",
+      " Developed MLOps pipelines for large dataset batch processing and managed metadata.",
+      " Deployed IBM Watsonx on IBM Cloud and Azure, exporting telemetry data to dashboards.",
+      " Devised and implemented automation solutions using Terraform, Vault, and Ansible.",
+      " Set up observability stacks (ELK, Prometheus/Grafana) and databases (PostgreSQL and DB2 databases).",
+      " Lead technical migration of legacy automotive products to new vehicle platforms.",
+      " Spearheaded Android Automotive platform adoption and created technical roadmaps.",
+      " Successfully migrated production systems from IBM Cloud to GitHub and Kubernetes on Azure.",
+      " Deployed in-vehicle services with Microsoft Azure cloud backends and secured API access.",
+      " Collaborated with vehicle partners to collect technical requirements andto develop software feature sets.",
+      " Worked closely with business managers to optimize value streams for various B2B products."
+    ]
+  },
+  {
+    id: 2,
+    title: "DevOps/SRE Engineer",
+    company: "REWE Digital GmbH",
+    duration: "(2016 - 2022)",
+    tasks: [
+      " Managed multi-cloud environment: Datacenter (PlusServer) and Google Cloud Platform (GCP).",
+      " Successfully migrated shop and supporting infrastructure to GCP (Lift and Shift).",
+      " Containerized services using Docker, set up repositories and orchestration with Nomad and Kubernetes (Rancher & GKE).",
+      " Implemented CI/CD pipelines using Ansible, Jenkins, and later migrated to GitHub Actions.",
+      " Set up monitoring and metrics with InfluxDB/Prometheus, Grafana, and Zabbix.",
+      " Setup centralized logging with ELK stack and managed Kafka/Zookeeper clusters.",
+      " Managed and migrated databases: MySQL, PostgreSQL, Redis and MongoDB.",
+      " Implemented Blue/Green and Canary deployments with service discovery and health monitoring.",
+      " Configured load balancers (Nginx/Apache) and reverse proxies (HAproxy/Traefik) for services.",
+      " Maintained Infrastructure-as-Code using Terraform and implemented secret management with HashiCorp Vault.",
+      " Developed applications and web services in Python, JavaScript, and Golang.",
+      " Participated in recruitment, interviewing, and onboarding of new developers."
+    ]
+  }
+]
